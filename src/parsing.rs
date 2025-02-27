97a121bbf01f7aeb6afda4017838281a9c6b8cfd
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-02-27 20:08:12 +0100

    Update extract_generics for correctness

--- src/parsing.rs
+++ src/parsing.rs
@@ -8,9 +8,49 @@ use syn::{
 };
 
 /// Given a [Generics](https://docs.rs/syn/latest/syn/struct.Generics.html), this function will
-/// return a [Punctuated](https://docs.rs/syn/latest/syn/punctuated/struct.Punctuated.html)
-/// whose elements are the input generics without their trait bounds and a
-/// [WhereClause](https://docs.rs/syn/latest/syn/struct.WhereClause.html) collecting that bounds.
+/// return:
+/// - A [Punctuated](https://docs.rs/syn/latest/syn/punctuated/struct.Punctuated.html) including
+/// the generics declarations without trait bounds.
+/// - Another `Punctuated` including the generics idents.
+/// - A [WhereClause](https://docs.rs/syn/latest/syn/struct.WhereClause.html) collecting the trait
+///   bounds.
+///
+/// The difference between the two first outputs is only meaningful if a generic const is included
+/// in the generics. While the first output will contain the whole const declaration, eg, `const N:
+/// usize`, the second one will just contain the const ident, eg, `N`. Extracting them in two
+/// different outputs is worthy as they have different use cases, as the snippets below show:
+///
+/// ```compile_fail
+/// struct SomeStruct<const N:usize>([bool; N]);
+///
+/// struct OtherStruct<const N: usize>{
+///     // This isn't allowed
+///     item: SomeStruct<const N: usize>
+/// }
+/// ```
+///
+///
+/// ```compile_fail
+/// struct SomeStruct<const N:usize>([bool; N]);
+///
+/// struct OtherStruct<const N: usize>{
+///     item: SomeStruct<N>
+/// }
+///
+/// // This cannot compile as N isn't correctly bounded
+/// impl<N> OtherStruct<N>{}
+/// ```
+///
+///
+/// ```
+/// struct SomeStruct<const N:usize>([bool; N]);
+///
+/// struct OtherStruct<const N: usize>{
+///     item: SomeStruct<N>
+/// }
+///
+/// impl<const N:usize> OtherStruct<N>{}
+/// ```
 ///
 /// # Example
 /// ```
@@ -24,41 +64,59 @@ use syn::{
 /// let where_clause: Option<WhereClause> = Some(parse_quote!{where D:From<String>});
 /// input = Generics{ where_clause, ..input};
 ///
-/// let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote!{'a,T,D,const N:usize};
+/// let output_declarations: Punctuated<GenericParam, Token![,]> = parse_quote!{'a, T, D, const N:
+/// usize};
+/// let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote!{'a,T,D,N};
 /// let output_where_clause = Some(parse_quote!{where D:From<String>, T: Config + Clone, D: Debug});
 ///
-/// // The first output contains the generics names, while the second one contains the where clause
-/// // with all the trait bounds.
-/// assert_eq!((output_idents, output_where_clause), rustilities::parsing::extract_generics(&input));
+/// assert_eq!((output_declarations, output_idents, output_where_clause), rustilities::parsing::extract_generics(&input));
 ///
 /// // A Generics without bounds
 /// let input: Generics = parse_quote! {
-///  <'a, T, D, const N:usize>
+///  <'a, T, D>
 /// };
 ///
-/// let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,const N:usize};
+/// let output_declarations: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D};
+/// let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D};
 ///
 /// // There's not a WhereClause returned
-/// assert_eq!((output_idents, None), rustilities::parsing::extract_generics(&input));
+/// assert_eq!((output_declarations, output_idents, None), rustilities::parsing::extract_generics(&input));
 /// ```
 pub fn extract_generics(
 	generics: &Generics,
-) -> (Punctuated<GenericParam, Token![,]>, Option<WhereClause>) {
+) -> (Punctuated<GenericParam, Token![,]>, Punctuated<GenericParam, Token![,]>, Option<WhereClause>)
+{
 	let mut where_clauses: Punctuated<WherePredicate, Token![,]> = Punctuated::new();
-	let generics_idents: Punctuated<GenericParam, Token![,]> = generics
+	let mut generics_idents: Punctuated<GenericParam, Token![,]> = Punctuated::new();
+	let generics_declarations: Punctuated<GenericParam, Token![,]> = generics
 		.params
 		.iter()
-		.map(|item| {
-			if let GenericParam::Type(generic_type) = item {
+		.map(|item| match item {
+			GenericParam::Type(generic_type) => {
 				let ident = &generic_type.ident;
 				let bounds = &generic_type.bounds;
 				if !bounds.is_empty() {
 					where_clauses.push(parse_quote! {#ident: #bounds});
 				}
-				GenericParam::Type(parse_quote! { #ident })
-			} else {
+				let ident = GenericParam::Type(parse_quote! { #ident });
+				generics_idents.push(ident.clone());
+				ident
+			},
+			GenericParam::Lifetime(lifetime) => {
+				let lifetime_dec = &lifetime.lifetime;
+				let bounds = &lifetime.bounds;
+				if !bounds.is_empty() {
+					where_clauses.push(parse_quote! {#lifetime_dec: #bounds});
+				}
+				let lifetime = GenericParam::Lifetime(parse_quote! {#lifetime_dec});
+				generics_idents.push(lifetime.clone());
+				lifetime
+			},
+			GenericParam::Const(generic_const) => {
+				let ident = &generic_const.ident;
+				generics_idents.push(GenericParam::Type(parse_quote! {#ident}));
 				item.clone()
-			}
+			},
 		})
 		.collect();
 
@@ -76,5 +134,5 @@ pub fn extract_generics(
 				None
 			}
 		});
-	(generics_idents, where_clause)
+	(generics_declarations, generics_idents, where_clause)
 }
--- src/parsing/tests.rs
+++ src/parsing/tests.rs
@@ -8,36 +8,47 @@ fn extract_generics_simple_generics() {
 		<'a, T, D, const N:usize>
 	};
 
-	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,const N:usize};
+	let output_declarations: Punctuated<GenericParam, Token![,]> =
+		parse_quote! {'a,T,D,const N:usize};
+	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,N};
 
-	assert_eq!((output_idents, None), extract_generics(&input));
+	assert_eq!((output_declarations, output_idents, None), extract_generics(&input));
 }
 
 #[test]
 fn extract_generics_with_bounds() {
 	let input: Generics = parse_quote! {
-		<'a, T: Config + Clone, D: Debug, const N:usize>
+		<'a, 'b: 'a, T: Config + Clone, D: Debug, const N:usize>
 	};
 
-	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,const N:usize};
-	let output_where_clause: WhereClause = parse_quote! {where T: Config + Clone, D: Debug};
+	let output_declarations: Punctuated<GenericParam, Token![,]> =
+		parse_quote! {'a, 'b,T,D,const N:usize};
+	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a, 'b,T,D,N};
+	let output_where_clause: WhereClause = parse_quote! {where 'b: 'a, T: Config + Clone, D: Debug};
 
-	assert_eq!((output_idents, Some(output_where_clause)), extract_generics(&input));
+	assert_eq!(
+		(output_declarations, output_idents, Some(output_where_clause)),
+		extract_generics(&input)
+	);
 }
 
 #[test]
 fn extract_generics_with_where_clause() {
 	let mut input: Generics = parse_quote! {
-		<'a, T, D, const N:usize>
+		<'a, T, D>
 	};
 
 	let where_clause: Option<WhereClause> = Some(parse_quote! {where D:From<String>});
 	input = Generics { where_clause, ..input };
 
-	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,const N:usize};
+	let output_declarations: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D};
+	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D};
 	let output_where_clause: WhereClause = parse_quote! {where D:From<String>};
 
-	assert_eq!((output_idents, Some(output_where_clause)), extract_generics(&input));
+	assert_eq!(
+		(output_declarations, output_idents, Some(output_where_clause)),
+		extract_generics(&input)
+	);
 }
 
 #[test]
@@ -49,9 +60,14 @@ fn extract_generics_with_bounds_and_where_clause() {
 	let where_clause: Option<WhereClause> = Some(parse_quote! {where D:From<String>});
 	input = Generics { where_clause, ..input };
 
-	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,const N:usize};
+	let output_declarations: Punctuated<GenericParam, Token![,]> =
+		parse_quote! {'a,T,D,const N:usize};
+	let output_idents: Punctuated<GenericParam, Token![,]> = parse_quote! {'a,T,D,N};
 	let output_where_clause: WhereClause =
 		parse_quote! {where D:From<String>, T: Config + Clone, D: Debug};
 
-	assert_eq!((output_idents, Some(output_where_clause)), extract_generics(&input));
+	assert_eq!(
+		(output_declarations, output_idents, Some(output_where_clause)),
+		extract_generics(&input)
+	);
 }
